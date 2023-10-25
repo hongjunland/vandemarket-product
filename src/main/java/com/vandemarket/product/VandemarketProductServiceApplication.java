868a850319f8abc9e3d1f2cc6df21c86f1c99050
hongjunland <zunza96@gmail.com>
2023-10-25 22:27:40 +0900

    first commit

---
+++ src/main/java/com/vandemarket/product/VandemarketProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vandemarket.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VandemarketProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VandemarketProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vandemarket/product/VandemarketProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vandemarket.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VandemarketProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
